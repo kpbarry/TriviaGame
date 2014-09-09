10af68f50b988e7881b4687fe528c143d3c31525
SeedPaths-User <lessdismissive@email.com>
2014-09-09 16:54:55 -0600

    Done

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -13,6 +13,45 @@ namespace TriviaGame
         {
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
+
+            Greet();
+
+            //Set up variables for trivia
+            int correct = 0;
+            int incorrect = 0;
+            int score = 0;
+
+            while (incorrect < 10)
+            {
+                Random rng = new Random();
+                int randomNum = rng.Next(1, 5001);
+
+                var triv = GetTriviaList()[randomNum];
+
+                Console.WriteLine(triv.Question);
+                Console.WriteLine("Your answer: ");
+                string input = Console.ReadLine();
+
+                if (input == triv.Answer.ToLower())
+                {
+                    correct++;
+                    score += 100;
+                    Console.WriteLine("Good guess!");
+                }
+                else if ((triv.Answer).Contains(input))
+                {
+                    correct++;
+                    score += 50;
+                    Console.WriteLine("Kinda right");
+                }
+                else
+                {
+                    incorrect++;
+                    Console.WriteLine("WRONG! The right answer was: " + triv.Answer);
+                }
+            }
+            Console.WriteLine("Score: " + score);
+            Console.ReadKey();
         }
 
 
@@ -29,8 +68,23 @@ namespace TriviaGame
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
+            //Go through every string (QandA) in contents
+            foreach (string s in contents)
+            {
+                //Declare newTrivia for every QandA string in contents
+                Trivia newTrivia = new Trivia(s);
+                returnList.Add(newTrivia);
+            }
+
             //Return the full list of trivia questions
             return returnList;
+        }
+
+        static void Greet()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + " you will answer questions until you get five wrong or hit Ctrl+C.");
         }
     }
 }
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -18,7 +18,22 @@ namespace TriviaGame
         // split the question from the answer in your
         // constructor and assign them to the Question
         // and Answer properties
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        //public string Category { get; set; }
+        
+        public Trivia(string splitting)
+        {
+            //Declare new list for splitting questions and answers
+            List<string> split = new List<string>();
+            //Split on *, index 0 is question, index 1 is answer
+            split = splitting.Split('*').ToList();
 
+            //Set index 0 to question
+            this.Question = split[0];
+            //Set index 1 to answer
+            this.Answer = split[1];
+        }
 
     }
 }
