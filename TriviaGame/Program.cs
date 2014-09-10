2a3d2e3289bdd34be97dca037a732fd54162b79f
kpbarry <kpbarry7@gmail.com>
2014-09-09 23:20:00 -0600

    Complete

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -13,7 +13,7 @@ namespace TriviaGame
         {
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
-
+            //Greet user with a separate function
             Greet();
 
             //Set up variables for trivia
@@ -21,39 +21,44 @@ namespace TriviaGame
             int incorrect = 0;
             int score = 0;
 
+            //Give user 10 wrong guesses
             while (incorrect < 10)
             {
+                //Random number generator to get question at random
                 Random rng = new Random();
                 int randomNum = rng.Next(1, 5001);
 
-                var triv = GetTriviaList()[randomNum];
+                //Set a variable called triviaQuestion to the question string
+                var triviaQuestion = GetTriviaList()[randomNum];
 
-                Console.WriteLine(triv.Question);
-                Console.WriteLine("Your answer: ");
+                //Trivia.Question will break up the string into a question and 
+                //display that to the user
+                Console.WriteLine(triviaQuestion.Question);
+                //Take in user input
+                Console.Write("Your answer: ");
                 string input = Console.ReadLine();
 
-                if (input == triv.Answer.ToLower())
+                //Convert triviaQuestion.Answer to lowercase and check if the answer is correct
+                if (input == triviaQuestion.Answer.ToLower())
                 {
                     correct++;
                     score += 100;
+                    Console.Clear();
                     Console.WriteLine("Good guess!");
                 }
-                else if ((triv.Answer).Contains(input))
-                {
-                    correct++;
-                    score += 50;
-                    Console.WriteLine("Kinda right");
-                }
+                //Wrong answer, tell user answer
                 else
                 {
                     incorrect++;
-                    Console.WriteLine("WRONG! The right answer was: " + triv.Answer);
+                    score -= 50;
+                    Console.Clear();
+                    Console.WriteLine(input + " is wrong!\nThe right answer is: " + triviaQuestion.Answer + "\n");
                 }
+                //Print correct and score
+                Console.WriteLine("Correct: " + correct + "\nScore: " + score + "\nYou have " + (10 - incorrect) + " incorrect guesses remaining.\n");
             }
-            Console.WriteLine("Score: " + score);
             Console.ReadKey();
         }
-
 
         //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
@@ -62,7 +67,7 @@ namespace TriviaGame
             List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
 
             //Each item in list "contents" is now one line of the Trivia.txt document.
-            
+
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
             // TODO: go through each line in contents of the trivia file and make a trivia object.
@@ -71,11 +76,10 @@ namespace TriviaGame
             //Go through every string (QandA) in contents
             foreach (string s in contents)
             {
-                //Declare newTrivia for every QandA string in contents
+                //Declare newTrivia for every string in contents, separated into question and answer
                 Trivia newTrivia = new Trivia(s);
                 returnList.Add(newTrivia);
             }
-
             //Return the full list of trivia questions
             return returnList;
         }
@@ -84,7 +88,7 @@ namespace TriviaGame
         {
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + " you will answer questions until you get five wrong or hit Ctrl+C.");
+            Console.WriteLine("Hello, " + name + " you will answer questions until you get 10 wrong or hit Ctrl+C.");
         }
     }
 }
