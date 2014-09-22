0fc85bdc4c254bdde1f68e372f041fbfc4412630
SeedPaths-User <lessdismissive@email.com>
2014-09-22 12:02:51 -0600

    Added highscores

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -57,7 +57,45 @@ namespace TriviaGame
                 //Print correct and score
                 Console.WriteLine("Correct: " + correct + "\nScore: " + score + "\nYou have " + (10 - incorrect) + " incorrect guesses remaining.\n");
             }
+            AddHighScore(correct);
+            DisplayHighScores();
             Console.ReadKey();
+        }
+
+        static void AddHighScore(int playerScore)
+        {
+            Console.WriteLine("Add your name to high scores list: ");
+            string playerName = Console.ReadLine();
+
+            //Create a gateway to the database
+            KevinEntities db = new KevinEntities();
+
+            //Create new high score object
+            HighScore newHighScore = new HighScore();
+            newHighScore.DateCreated = DateTime.Now;
+            newHighScore.Game = "Trivia";
+            newHighScore.Name = playerName;
+            newHighScore.Score = playerScore;
+
+            //Add it to the database
+            db.HighScores.Add(newHighScore);
+
+            //Save changes to db
+            db.SaveChanges();
+        }
+
+        static void DisplayHighScores()
+        {
+            Console.Clear();
+            Console.WriteLine("Trivia High Scores");
+            Console.WriteLine("-----------------------------");
+
+            KevinEntities db = new KevinEntities();
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Trivia").OrderBy(x => x.Score).Take(10).ToList();
+            foreach (HighScore i in highScoreList)
+            {
+                Console.WriteLine("{0}. {1} - {2} - {3}", highScoreList.IndexOf(i) + 1, i.Name, i.Score, i.DateCreated.Value.ToShortDateString());
+            }
         }
 
         //This functions gets the full list of trivia questions from the Trivia.txt document
